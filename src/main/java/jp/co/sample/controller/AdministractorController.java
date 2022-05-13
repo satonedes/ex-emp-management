1f61c558067e789835aa7d15009fe5497a118158
itosatone <satone.ito@rakus-partners.co.jp>
2022-05-13 13:57:47 +0900

    administractor controller

--- src/main/java/jp/co/sample/controller/AdministractorController.java
+++ src/main/java/jp/co/sample/controller/AdministractorController.java
@@ -1,5 +1,27 @@
 package jp.co.sample.controller;
 
-public class AdministractorController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import jp.co.sample.form.InsertAdministractorForm;
+import jp.co.sample.service.AdministractorService;
+
+@Controller
+@RequestMapping("/")
+public class AdministractorController {
+	
+	@Autowired
+	private AdministractorService administractorService;
+	
+	@ModelAttribute
+	public InsertAdministractorForm setUpInsertAdministractorForm() {
+		InsertAdministractorForm adForm = new InsertAdministractorForm();
+		return adForm;
+	}
+	@RequestMapping("/toInsert")
+	public String toInsert() {
+		return "administrator";
+	}
 }
