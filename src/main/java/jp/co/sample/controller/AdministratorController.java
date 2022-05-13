344ec1d82308e2949e35d61208b7b556d9f06a66
itosatone <satone.ito@rakus-partners.co.jp>
2022-05-13 17:58:30 +0900

    login.html

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -1,8 +1,11 @@
 package jp.co.sample.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -39,8 +42,22 @@ public class AdministratorController {
 		return new LoginForm();
 //		return setUploginForm();
 	}
-	@RequestMapping("/login")
+	@RequestMapping("/")
 	public String toLogin() {
 		return "administrator/login";
+	}
+	@Autowired
+	private HttpSession session;
+	
+	@RequestMapping("/login")
+	public String login(LoginForm form, Model model) {
+		Administrator administrator = administratorService.login(form.getMailAddress(), form.getPassword());
+		if(administrator == null) {
+			model.addAttribute("form", form);
+			return "administrator/login";
+		}else {
+			session.setAttribute("administratorName", administrator.getName());
+		}
+		return "forword:/employee/showList";
 	}
 }
