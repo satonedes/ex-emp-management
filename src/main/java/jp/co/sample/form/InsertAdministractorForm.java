7a0b5645bda6441a42c91e1690b8e026e2abae15
itosatone <satone.ito@rakus-partners.co.jp>
2022-05-13 14:09:16 +0900

    insert.html

--- src/main/java/jp/co/sample/form/InsertAdministractorForm.java
+++ src/main/java/jp/co/sample/form/InsertAdministractorForm.java
@@ -1,5 +1,30 @@
 package jp.co.sample.form;
 
 public class InsertAdministractorForm {
+	public String name;
+	public String mail;
+	public String password;
+	@Override
+	public String toString() {
+		return "InsertAdministractorForm [name=" + name + ", mail=" + mail + ", password=" + password + "]";
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 }
