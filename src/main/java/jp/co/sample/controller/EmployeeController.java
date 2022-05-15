fd794ddaffce8eb2f4d4d48203d6de97c44b33e0
itosatone <satone.ito@rakus-partners.co.jp>
2022-05-15 15:27:38 +0900

    detail.html

--- src/main/java/jp/co/sample/controller/EmployeeController.java
+++ src/main/java/jp/co/sample/controller/EmployeeController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jp.co.sample.domain.Employee;
+import jp.co.sample.form.UpdateEmployeeForm;
 import jp.co.sample.service.EmployeeService;
 
 @Controller
@@ -24,6 +26,19 @@ public class EmployeeController {
 		model.addAttribute("employeeList", employeeList);
 		
 		return "employee/list";
+	}
+	
+	@ModelAttribute
+	public UpdateEmployeeForm setUpdateEmployeeForm() {
+		return new UpdateEmployeeForm();
+	}
+	
+	@RequestMapping("/showDetail")
+	public String showDetail(String id,Model model) {
+		int depId = Integer.parseInt(id);
+		Employee employee = employeeService.showDetail(depId);
+		model.addAttribute("employee", employee);
+		return "employee/detail";
 	}
 
 }
