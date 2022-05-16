6487a69b9d56d285ae568e47e028e7f70740b5fa
itosatone <satone.ito@rakus-partners.co.jp>
2022-05-16 09:54:56 +0900

    detail.html dependentsCount

--- src/main/java/jp/co/sample/controller/EmployeeController.java
+++ src/main/java/jp/co/sample/controller/EmployeeController.java
@@ -40,5 +40,14 @@ public class EmployeeController {
 		model.addAttribute("employee", employee);
 		return "employee/detail";
 	}
+	@RequestMapping("/update")
+	public String update(UpdateEmployeeForm form) {
+		Employee employee = employeeService.showDetail(Integer.parseInt(form.getId()));
+		employee.setDependentsCount(Integer.parseInt(form.getDependentsCount()));
+		employeeService.update(employee);
+		
+		return "redirect:/employee/showList";
+		
+	}
 
 }
--- src/main/java/jp/co/sample/repository/EmployeeRepository.java
+++ src/main/java/jp/co/sample/repository/EmployeeRepository.java
@@ -39,7 +39,7 @@ public class EmployeeRepository {
 	}
 	public void update(Employee employee) {
 		SqlParameterSource param = new BeanPropertySqlParameterSource(employee);
-		String updateSql = "update employees set name =:name, image =:image, gender =:gender,hireDate =:hireDate,mail_Adress =:mailAddress, zipCode =:zipCode, address =:address, telephone =:telephone, salary =:salary,characteristics =:characteristics, dependentsCount =:dependentsCount where id =:id";
+		String updateSql = "update employees set name =:name, image =:image, gender =:gender, hire_date =:hireDate, mail_address =:mailAddress, zip_code =:zipCode, address =:address, telephone =:telephone, salary =:salary, characteristics =:characteristics, dependents_count =:dependentsCount where id =:id";
 		template.update(updateSql, param);
 	}
 	
